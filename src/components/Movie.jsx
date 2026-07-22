3b281ed044398b5a915561ed19cd016b6870f097
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-07-22 14:05:31 +1000

    Added add to favourites modal and remove from favourites feature

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,15 +1,48 @@
-import Button from './Button';
+// import Button from './Button';
+import { useState } from "react";
 
 const Movie = ({movietitle = "Movie Title", poster = "https://picsum.photos/300/450"}) => {
+
+    const [favourites, setFavourites] = useState(false);
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleSubmit = () => {
+        setFavourites(true);
+        setSubmitted(true);
+    }
+
+    const closeModal = () => {
+        setSubmitted(false);
+    }
+
+    const removeFavourites = () => {
+        setFavourites(false);
+    }
+
     return ( 
         <>
             <article className="movie-card">
                 <img className="movie-image" src={poster} alt={movietitle} />
                 <div className="movie-content">
                     <p className="movie-title">{movietitle}</p>
-                    <Button text='Add to favourites'/>
+                    <button 
+                        className={`movie-btn ${favourites ? 'added': ''}`} 
+                        onClick={favourites ? removeFavourites : handleSubmit}
+                    >&#9825; {favourites ? 'Remove from Favourites': 'Add to Favourites'}</button>
+                    {/* <Button text='Add to favourites'/> */}
                 </div>
             </article>
+            {/* modal */}
+            {
+                submitted && 
+                <div className="modal-overlay">
+                    <div className="modal">
+                        <h2>Added to favourites</h2>
+                        <p>You added {movietitle} to favourites.</p>
+                        <button className="close-btn" onClick={closeModal}>Close</button>
+                    </div>
+                </div>
+            }
         </>
      );
 }
